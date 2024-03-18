5e4ca273993eeb2bec003398f3742a33f68d2588
elir-mahad <mmahad280@gmail.com>
2024-03-17 22:21:08 -0400

    style(dayworkout): improve jsx and css

--- src/components/DayWorkout/DayWorkout.js
+++ src/components/DayWorkout/DayWorkout.js
@@ -5,15 +5,32 @@ const DayWorkout = ({ day, exercises }) => {
   //
   return (
     <div className="day-workout-container">
-      <h2>{day}</h2>
+      <h2 className="day-heading">{day}</h2>
       {exercises.map((exercise, index) => (
         <div className="exercise-container" key={index}>
-          <h3>{exercise.name}</h3>
-          <img
-            className="exercise-image"
-            src={exercise.photo} // Example image source
-            alt={exercise.name}
-          />
+          <h3 className="exercise-name">{exercise.name}</h3>
+          <div className="exercise-details">
+            <img
+              className="exercise-image"
+              src={exercise.photo} // Example image source
+              alt={exercise.name}
+            />
+            <div className="info-container">
+              {exercise.muscle_groups && (
+                <p>
+                  <strong>Muscle Groups:</strong>{" "}
+                  {exercise.muscle_groups.join(", ")}
+                </p>
+              )}
+
+              <p>
+                <strong>Equipment:</strong> {exercise.equipment}
+              </p>
+              <p>
+                <strong>Exercise Type:</strong> {exercise.exercise_type}
+              </p>
+            </div>
+          </div>
           <ul className="instruction-list">
             {exercise.instructions.map((instruction, i) => (
               <li key={i}>{instruction}</li>
--- src/components/Filters/ByDay/ByDay.js
+++ src/components/Filters/ByDay/ByDay.js
@@ -11,6 +11,11 @@ const ByDay = () => {
     ...lowerBodyData,
   ];
 
+  // function extractExerciseNames(exercises) {
+  //   return exercises.map((exercise) => exercise.name);
+  // }
+  // const exerciseNames = extractExerciseNames(exercises);
+  // console.log(exerciseNames);
   //! Filter based on day
 
   const [selectedDay, setSelectedDay] = useState(null);
@@ -34,3 +39,75 @@ const ByDay = () => {
 };
 
 export default ByDay;
+
+// [
+//   "Bench Press",
+//   "Incline dumbell press",
+//   "Skull Crushers",
+//   "Cable flyes",
+//   "Tricep pushdowns",
+//   "Tricep Dips",
+//   "Pull-Up",
+//   "Barbell Row",
+//   "Dumbbell Shoulder Press",
+//   "Dumbbell Bicep Curl",
+//   "Tricep kickbacks",
+//   "Dumbbell Flyes",
+//   "Military Press",
+//   "Lat Pulldown",
+//   "Chest Press Machine",
+//   "Hammer Curls",
+//   "Seated Row Machine",
+//   "Arnold Press",
+//   "Lateral Raises",
+//   "Front Dumbbell Raises",
+//   "Pushdowns",
+//   "Reverse Flyes",
+//   "Reverse curls",
+//   "Barbell Shoulder press",
+//   "Push Press",
+//   "Tricep overhead dumbbell extension",
+//   "Preacher Curls",
+//   "Upright Rows",
+//   "Cable Rows",
+//   "Face Pulls",
+//   "Hammer Strength Bench Press",
+//   "Spider Curls",
+//   "Dumbbell Pullover",
+//   "Cable Crossover",
+//   "Close-Grip Bench Press",
+//   "Dumbbell Shrugs",
+//   "Squats",
+//   "Lunges",
+//   "Deadlifts",
+//   "Romanian Deadlifts",
+//   "Bulgarian Split Squats",
+//   "Leg Press",
+//   "Step-Ups",
+//   "Calf Raises",
+//   "Hip Thrusts",
+//   "Leg Curls",
+//   "Barbell Hip Thrusts",
+//   "Hack Squats",
+//   "Sumo Deadlifts",
+//   "Dumbbell Lunges",
+//   "Dumbbell Step-Ups",
+//   "Seated Leg Press",
+//   "Walking Lunges",
+//   "Single-Leg Romanian Deadlifts",
+//   "Barbell Reverse Lunges",
+//   "Good Mornings",
+//   "Smith Machine Squats",
+//   "Leg Extensions",
+//   "Glute Bridges",
+//   "Cable Pull-Throughs",
+//   "Box Jumps",
+//   "Reverse Hyperextensions",
+//   "Barbell Box Squats",
+//   "Single-Leg Press",
+//   "Cossack Squats",
+//   "Dumbbell Deadlifts",
+//   "Reverse Lunges",
+//   "Seated Calf Raises",
+//   "Stiff-Legged Deadlifts",
+// ];
